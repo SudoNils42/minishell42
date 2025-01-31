a9249f0528d7844658f41dcde714310a5237584b
Nils Bonnet <nbonnet@c2r8s5.42lausanne.ch>
2025-01-31 14:23:59 +0100

    fafafa

--- n_exec_builtins.c
+++ n_exec_builtins.c
@@ -6,7 +6,7 @@
 /*   By: nbonnet <nbonnet@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 15:55:53 by nbonnet           #+#    #+#             */
-/*   Updated: 2025/01/30 16:16:56 by nbonnet          ###   ########.fr       */
+/*   Updated: 2025/01/31 14:23:47 by nbonnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 void	exec_builtins(t_data *data)
 {
-	if (ft_strncmp(data->command->args[0], "pwd", 3) == 0)
+	if (ft_strcmp(data->command->args[0], "pwd") == 0)
 		data->exit_status = ft_pwd();
-	if (ft_strncmp(data->command->args[0], "env", 3) == 0)
+	else if (ft_strcmp(data->command->args[0], "env") == 0)
 		data->exit_status = ft_env(data);
-	if (ft_strncmp(data->command->args[0], "echo", 4) == 0)
+	else if (ft_strcmp(data->command->args[0], "echo") == 0)
 		data->exit_status = ft_echo(data);
-	if (ft_strncmp(data->command->args[0], "cd", 2) == 0)
+	else if (ft_strcmp(data->command->args[0], "cd") == 0)
 		data->exit_status = ft_cd(data);
-	if (ft_strncmp(data->command->args[0], "export", 6) == 0)
+	else if (ft_strcmp(data->command->args[0], "export") == 0)
 		data->exit_status = ft_export(data);
-	if (ft_strncmp(data->command->args[0], "unset", 5) == 0)
+	else if (ft_strcmp(data->command->args[0], "unset") == 0)
 		data->exit_status = ft_unset(data);
-	if (ft_strncmp(data->command->args[0], "exit", 4) == 0)
+	else if (ft_strcmp(data->command->args[0], "exit")== 0)
 		data->exit_status = ft_exit();
 }
