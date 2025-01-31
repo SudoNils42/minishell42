9a48538908b7c79a0729234259c6ff997cd40314
Raul Batista <rabatist@c2r8s3.42lausanne.ch>
2025-01-31 16:24:48 +0100

    norminetted

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: rabatist <rabatist@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 16:14:19 by nbonnet           #+#    #+#             */
-/*   Updated: 2025/01/31 16:06:22 by rabatist         ###   ########.fr       */
+/*   Updated: 2025/01/31 16:24:30 by rabatist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,12 +85,12 @@ int				get_token_type(char *token);
 
 // exec_utils.c
 void			wait_for_children(t_data *data);
-void    run_child_process(t_data *data, char *cmd_path, int is_builtin);
+void			run_child_process(t_data *data, char *cmd_path, int is_builtin);
 void			cleanup_parent(t_data *data);
 void			setup_pipe(t_data *data, int pipe_fd[2]);
 void			prepare_pipe_connection(t_data *data);
-void    apply_redirections(t_data *data);
-void    restore_redirections(t_data *data);
+void			apply_redirections(t_data *data);
+void			restore_redirections(t_data *data);
 
 // exec.c
 int				parse_command(t_data *data);
@@ -101,6 +101,7 @@ void			handle_previous_pipe(t_data *data);
 // command_path.c
 char			*find_command_path(char *cmd, t_data *data);
 char			*ft_strjoin_with_slash(const char *s1, const char *s2);
+char			*find_path(t_data *data);
 
 // utils.c
 int				ft_strcmp(char *s1, char *s2);
@@ -114,10 +115,11 @@ int				handle_redirection(t_data *data);
 int				if_word(t_data *data);
 int				if_pipe(t_token token);
 
-void	exec_builtins(t_data *data);
-int	is_builtin(t_data *data);
-// builtins
+void			exec_builtins(t_data *data);
+int				is_builtin(t_data *data);
 
+// BUILTINS
+//cd.c
 int				ft_cd(t_data *data);
 int				ft_cd2(t_data *data);
 void			update_old_pwd_env(t_data *data);
@@ -127,14 +129,14 @@ void			update_old_pwd_exp2(t_data *data, char *pwd);
 void			update_pwd_env(t_data *data);
 void			update_pwd_exp(t_data *data);
 char			*ft_get_home(t_data *data);
-
+//echo.c
 int				ft_echo(t_data *data);
 void			ft_echo_print(t_data *data, int i, int line);
-
+//env.c
 int				ft_env(t_data *data);
-
+//exit.c
 int				ft_exit(void);
-
+//export.c
 void			bubble_sort(char **env);
 void			valid_var_name(t_data *data, int i);
 int				is_valid_var_name(char *var);
@@ -143,24 +145,26 @@ void			update_env_with_equal(t_data *data, char *str);
 void			update_exp_with_equal(t_data *data, char *str);
 void			update_exp_without_equal(t_data *data, char *str);
 int				ft_export(t_data *data);
-
+//pwd.c
 int				ft_pwd(void);
-
+//unset.c
 int				ft_unset(t_data *data);
 void			unset_var(t_data *data, char *var_name);
 void			unset_var_in_exp(t_data *data, char *var_name);
 
-//
-void	make_env(t_data *data, char **env);
-void	make_exp(t_data *data, char **env);
-char	*find_path(t_data *data);
-void	signals(void);
-void	sig_int(int signal);
-int		check_args(char *str);
-int		is_quotes_open(char *str);
-void	check_dollars(t_data *data);
-void	get_exit_status(t_data *data, int i);
-char	*get_env_value(char *str, t_data *data);
-char	*ft_get_home(t_data *data);
+//parse.c
+int				check_args(char *str);
+int				is_quotes_open(char *str);
+void			check_dollars(t_data *data);
+void			get_exit_status(t_data *data, int i);
+char			*get_env_value(char *str, t_data *data);
+
+//signals.c
+void			signals(void);
+void			sig_int(int signal);
+
+//make_env.c
+void			make_env(t_data *data, char **env);
+void			make_exp(t_data *data, char **env);
 
 #endif
